af3731297242f33e672f5664d362d8f3ccd20e50
Lucian45T <155108954+Lucian45T@users.noreply.github.com>
2024-01-25 22:42:41 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function FoundButton() {
+    document.getElementById('modify').innerHTML = 'castigator'
+   
+}
+function FoundButton2() {
+    document.getElementById('modify2').innerHTML = 'necastigator'
+}
+function FoundButton3() {
+    document.getElementById('modify3').innerHTML = 'necastigator'
+}
